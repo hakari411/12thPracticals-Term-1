3b6d46f576f0bae4d4342dbce029c22f9b3c784f
hakari411 <73415318+hakari411@users.noreply.github.com>
2021-09-08 23:36:55 +0530

    Update question_5.py

--- question_5.py
+++ question_5.py
@@ -22,3 +22,60 @@ def binary_search(arr,l,r,x):
 def binary_search_main(arr, x):
     return binary_search(sorted(arr),0,len(arr)-1,x)
 
+
+
+
+#alternate
+def linear_search(list1,c) :
+    for a in list1 :
+        if a==c :
+            return str(c)+" was found in the list via linear search "
+            break
+    else :
+        return str(c) + " does not exist in list"
+
+
+def binary_search(list1,c) :
+    z=0
+    list1.sort()
+    while(z<(len(list1)/2)) :
+        a=int((len(list1)/2))
+        if c==list1[a] :
+            return str(c)+" was found in the list via binary search "
+            break
+        elif (c>list1[a]) :
+            list1=list1[a:len(list1)]
+            binary_search(list1,c)
+            z+=1
+        elif (c<list1[a]) :
+            list1=list1[0:a]
+            binary_search(list1,c)
+            z+=1
+    else :
+        return str(c) + " does not exist in list"
+
+        
+    
+#__main__
+n=input("Enter numeric values seperated by spaces : ").split()
+r=int(input("Enter value required : "))
+for i in range(0,len(n)) :
+    n[i]=int(n[i])
+print(binary_search(n,r))
+print(linear_search(n,r))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
