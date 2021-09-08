68e5cc978e8655f54bf3bcff36050f864523f235
hakari411 <73415318+hakari411@users.noreply.github.com>
2021-09-08 19:13:32 +0530

    Update question_2.py

--- question_2.py
+++ question_2.py
@@ -4,3 +4,13 @@ import random
 def get_random_n(n):
     return random.randint(10**(n-1), 10**(n) -1)
 print(get_random_n(3))
+
+
+
+#alternate
+def n_digit_number(n) :
+    return int(random.random()*(10**n))
+
+n=int(input("Enter  a number : "))
+a=n_digit_number(n)
+print("The number generated is : ",a)
