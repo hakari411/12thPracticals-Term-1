f5611ec7c2c313a92d029ea05d9fd7716a45e586
hakari411 <73415318+hakari411@users.noreply.github.com>
2021-09-08 19:10:04 +0530

    Update question 1.py

--- question 1.py
+++ question 1.py
@@ -10,3 +10,31 @@ def factorial(n):
 #Sum of the first n even numbers
 def sum_first_even(n):
     return (n*(n+1))
+
+
+#alterate
+
+#part 1
+def factorial(a) :
+    b=1
+    while(a!=0) :
+        b*=a
+        a-=1
+    return b
+
+a=int(input("Enter a number : "))
+c=factorial(a)
+print("Required factorial is = ",c)
+
+#part 2
+def sum_of_even(n) :
+    w=0
+    x=0
+    while(w<n) :
+        x+=2*w
+        w+=1
+    return x
+
+n=int(input("Enter a number : "))
+y=sum_of_even(n)
+print("Sum of first ",n," even numbers is = ",y
