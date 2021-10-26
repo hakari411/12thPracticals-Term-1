7bb33d71c450d295f95500dbc0ce101c13fd4d66
hakari411 <73415318+hakari411@users.noreply.github.com>
2021-10-26 15:00:43 +0530

    Update question_11.py
    added a tested alternate to q11

--- question_11/question_11.py
+++ question_11/question_11.py
@@ -20,3 +20,45 @@ def show_records():
             print(i)
             count += 1
     print("The count of people above 75%  ", count)
+
+    
+    
+    #alternate
+    
+import pickle
+finalL=[]
+
+
+def CreateFile() :
+     fbw=open("student.dat","wb+") # remeber to write ab+ not wb +
+     truthval= input("Do you want to enter records ? (y/n) : ")
+     while True :
+         if truthval == "y" or truthval== "Y" :
+             admn_no = int(input("enter admission number : "))
+             name = input("enter name : ")
+             percentage = int(input("enter percentage : "))
+             tempL = [admn_no, name, percentage]
+             finalL.append(tempL)
+             truthval= input("Do you want to enter records ? (y/n) : ")
+         elif truthval == "n" or truthval== "N" :
+             pickle.dump(finalL,fbw)
+             fbw.close()
+             break
+def CountRec() :
+    global count
+    count=0
+    fb=open("student.dat","rb+")
+    fbr=pickle.load(fb)
+    for i in range(0,len(finalL)) :
+        if fbr[i][2] > 75 :
+            count+=1
+            print("Admission Number : ",finalL[i][0]," | Name : ", finalL[i][1]," | Percentage : ",finalL[i][2])
+        else:
+            break
+    print("Total number of students with percentage > 75 : ",count)
+    fb.close()
+    
+                   
+    
+CreateFile()
+CountRec()
