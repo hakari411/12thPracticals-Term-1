ba7554b72eff88a010ff0df8db67ef64b8885b33
hakari411 <73415318+hakari411@users.noreply.github.com>
2021-10-26 14:19:08 +0530

    Update question_10.py
    added an alternate to question 10. Some cases not tested

--- question_10/question_10.py
+++ question_10/question_10.py
@@ -68,3 +68,50 @@ def search_record(author):
 # add_record()
 # print(get_data())
 # print(search_record("R.K. Narayan"))
+
+
+
+
+
+
+#alternate
+import pickle
+finalL=[]
+def CreateFile() :
+     fbw=open("book.dat","ab+")
+     truthval= input("Do you want to enter records ? (y/n) : ")
+     while True :
+         if truthval == "y" or truthval== "Y" :
+             bookno = int(input("enter bookno : "))
+             bookname = input("enter bookname : ")
+             author = input("enter author : ")
+             price = float(input("enter price : "))
+             tempL = [bookno, bookname, author, price]
+             fbw.seek(0)
+             finalL.append(tempL)
+             truthval= input("Do you want to enter records ? (y/n) : ")
+         elif truthval == "n" or truthval== "N" :
+             pickle.dump(finalL,fbw)
+             fbw.close()
+             print("file created")
+             break
+        
+def CountRec(Author) :
+     authsearch=Author
+     fb = open("book.dat","rb+")
+     fbr=pickle.load(fb)
+     print(fbr)
+     tval = input("Do you want to search records ? (y/n) : ")
+     count=0
+     if tval == "y" or tval== "Y" :
+         for i in range(0,len(fbr)) :
+             if fbr[i][2]==authsearch :
+                      count+=1     
+         print(authsearch," has published ",count," books")
+         fb.close()
+     elif tval == "n" or tval== "N" :
+         fb.close()
+         
+              
+CreateFile()
+CountRec("whatever you want")
